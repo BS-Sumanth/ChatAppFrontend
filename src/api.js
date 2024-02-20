037b35977891deaabed9fa14fb2aeae83ea16fa2
BS-Sumanth <21cse033@bnmit.in>
2024-02-20 23:14:35 +0530

    updated api

--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { logout } from "./shared/utils/auth";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:5002/api",
+    //baseURL: "http://localhost:5002/api",
+    baseURL: "chat-app-backend-azure.vercel.app",
     timeout: 1000,
 });
 
