55e6b4ff09900f350c461690147593df2a3dff20
BS-Sumanth <117374943+BS-Sumanth@users.noreply.github.com>
2024-07-15 20:19:45 +0530

    Update socketConnection.js

--- src/realtimeCommunication/socketConnection.js
+++ src/realtimeCommunication/socketConnection.js
@@ -14,7 +14,7 @@ let socket = null;
 export const connectWithSocketServer = (userDetails) => {
     const jwtToken = userDetails.token;
 
-    socket = io("http://localhost:5002", {
+    socket = io("https://chatappbackend-u393.onrender.com", {
         auth: {
             token: jwtToken,
         },
